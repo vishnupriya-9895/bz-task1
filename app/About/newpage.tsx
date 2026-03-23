1ce7951a8725299bb8d8c0cd69473d0f33b06f36
vishnupriya-9895 <priyavishnu32934@gmail.com>
2026-03-23 23:43:37 +0530

    new

---
+++ app/About/newpage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const newpage = () => {
+  return (
+    <div><h1>example page </h1>
+    <img src="" alt="" />
+    <h1>hey</h1>
+    </div>
+  )
+}
+
+export default newpage
